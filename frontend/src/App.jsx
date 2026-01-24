3796f5a918349aeebd99ae6056f8fd5d90ab3d4e
ahany42 <ahanyfathy42@gmail.com>
2026-01-24 20:22:06 +0200

    add: navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,9 +2,11 @@ import { Route, Routes } from "react-router-dom";
 import HomePage from "./Components/Pages/HomePage/HomePage.jsx";
 import "./App.css";
 import ChatWidget from "./Components/Other/ChatWidget/ChatWidget.jsx";
+import NavBar from "./Components/Sections/NavBar/NavBar.jsx";
 function App() {
   return (
     <>
+      <NavBar />
       <Routes>
         <Route path="/" element={<HomePage />} />{" "}
       </Routes>
---
+++ frontend/src/Components/Sections/NavBar/NavBar.jsx
@@ -0,0 +1,43 @@
+import { TabNav, Button } from "@radix-ui/themes";
+import { IoHome } from "react-icons/io5";
+import { MdFileUpload } from "react-icons/md";
+import { IoIosStats } from "react-icons/io";
+import { useNavigate, useLocation } from "react-router-dom";
+import "./NavBar.css";
+const NavBar = () => {
+  const navigate = useNavigate();
+  const location = useLocation();
+  return (
+    <TabNav.Root size="2" color="plum" className="navbar">
+      <div className="navbar-links">
+        <TabNav.Link
+          onClick={() => navigate("/")}
+          active={location.pathname === "/"}
+          className={location.pathname === "/" && "primary-bg"}
+        >
+          <IoHome /> Home
+        </TabNav.Link>
+        <TabNav.Link
+          onClick={() => navigate("/upload-dataset")}
+          active={location.pathname === "/upload-dataset"}
+          className={location.pathname === "/upload-dataset" && "primary-bg"}
+        >
+          <MdFileUpload />
+          Upload Dataset
+        </TabNav.Link>
+        <TabNav.Link
+          onClick={() => navigate("/dashboard")}
+          active={location.pathname === "/dashboard"}
+          className={location.pathname === "/dashboard" && "primary-bg"}
+        >
+          <IoIosStats /> Dashboard
+        </TabNav.Link>
+      </div>
+      <Button color="Gray" variant="surface">
+        Get Started
+      </Button>
+    </TabNav.Root>
+  );
+};
+
+export default NavBar;
